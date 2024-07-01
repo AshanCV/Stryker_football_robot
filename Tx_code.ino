ebed73eaf17fe2d37066098a45eecacc6d9a26ef
Ashan Vithanage <153393247+AshanCV@users.noreply.github.com>
2024-07-01 23:31:02 +0530

    Update and rename Tx_code to Tx_code.ino

---
+++ Tx_code.ino
@@ -0,0 +1,22 @@
+/* Stryker - Transmitter 
+   01.07.2024
+   Programme for Radio controlled car with 4 mecanum wheels.
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+
+
+void setup (){
+
+
+}
+
+void loop(){
+
+
+
+}
